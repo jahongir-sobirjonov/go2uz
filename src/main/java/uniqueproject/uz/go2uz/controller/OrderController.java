dc94d2e000d61da91d00432b0faa003993574d0e
jahongir-sobirjonov <sjahongir221@gmail.com>
2024-06-12 15:42:22 +0500

    a lot of classes and futures

--- src/main/java/uniqueproject/uz/go2uz/controller/OrderController.java
+++ src/main/java/uniqueproject/uz/go2uz/controller/OrderController.java
@@ -1,21 +1,33 @@
 package uniqueproject.uz.go2uz.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import uniqueproject.uz.go2uz.dto.auth.request.OrderRequest;
 import uniqueproject.uz.go2uz.dto.auth.response.OrderResponse;
+import uniqueproject.uz.go2uz.entity.Order;
+import uniqueproject.uz.go2uz.entity.enums.OrderStatus;
 import uniqueproject.uz.go2uz.service.OrderService;
+
+import java.util.UUID;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/orders")
 public class OrderController {
     private final OrderService orderService;
+    private final ModelMapper modelMapper;
 
     @PostMapping("/order-tour")
     public ResponseEntity<String> buyTour(@RequestBody OrderRequest orderRequest) {
         return ResponseEntity.status(HttpStatus.CREATED).body(orderService.orderTour(orderRequest));
+    }
+
+    @PatchMapping("/update-order-status/{orderId}")
+    public ResponseEntity<OrderResponse> updateOrderStatus(@PathVariable UUID orderId, @RequestBody OrderStatus status) {
+        return ResponseEntity.ok(orderService.updateOrderStatus(orderId, status));
     }
 
 
