071233f84d1ff337d3e55bc3679c8a5fcb48f6b1
jahongir-sobirjonov <sjahongir221@gmail.com>
2024-06-19 17:09:03 +0500

    OrderRepository and build.gradle

--- src/main/java/uniqueproject/uz/go2uz/repository/OrderRepository.java
+++ src/main/java/uniqueproject/uz/go2uz/repository/OrderRepository.java
@@ -1,14 +1,26 @@
 package uniqueproject.uz.go2uz.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import uniqueproject.uz.go2uz.entity.Order;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 @Repository
 public interface OrderRepository extends JpaRepository<Order, UUID> {
     List<Order> findByTour_Agency_Id(UUID agencyId);
+
+    @Query("SELECT o FROM orders o WHERE o.orderDate BETWEEN :startDate AND :endDate " +
+            "AND (:travelTypes IS NULL OR o.tour.category IN :travelTypes) " +
+            "AND (:bookingStatuses IS NULL OR o.status IN :bookingStatuses)")
+    List<Order> findOrdersBetweenDates(
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate,
+            @Param("travelTypes") List<String> travelTypes,
+            @Param("bookingStatuses") List<String> bookingStatuses);
 
 }
