cf75c73758ce916e137f4b15b1658f0d45d3a78b
jahongir-sobirjonov <sobirjonovjahongir133@gmail.com>
2024-06-26 12:46:37 +0500

    Dockerfilew

--- src/main/java/uniqueproject/uz/go2uz/chats/chat/ChatMessageRepository.java
+++
@@ -1,9 +0,0 @@
-package uniqueproject.uz.go2uz.chats.chat;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.List;
-
-public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
-    List<ChatMessage> findByChatId(String chatId);
-}
--- src/main/java/uniqueproject/uz/go2uz/chats/user/Status.java
+++
@@ -1,5 +0,0 @@
-package uniqueproject.uz.go2uz.chats.user;
-
-public enum Status {
-    ONLINE, OFFLINE
-}
