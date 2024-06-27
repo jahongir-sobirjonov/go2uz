324b4e57ad9475fc4f487db8a4717b585008efc1
jahongir-sobirjonov <sobirjonovjahongir133@gmail.com>
2024-06-27 13:18:18 +0500

    s

--- startup.sh
+++ startup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #nohup java -jar /path/to/app/hello-world.jar > /path/to/log.txt 2>&1 &
 
-nohup ./mvnw spring-boot:run > log.txt 2>&1 &
+nohup ./gradlew bootRun > log.txt 2>&1 &
 echo $! > ./pid.file
+
+
