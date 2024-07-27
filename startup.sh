6e72beb304e2ae46f11d21300ad7acdc4216f0ec
Jahongir <sobirjonovjahongir133@gmail.com>
2024-07-27 21:00:28 +0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run the Spring Boot application using Gradle and save logs to log.txt
-nohup ./gradlew bootRun > /path/to/log.txt 2>&1 &
+nohup ./gradlew bootRun > /log.txt 2>&1 &
 
 # Save the process ID (PID) to a file for later use
 echo $! > /path/to/pid.file
